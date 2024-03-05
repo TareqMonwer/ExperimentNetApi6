5cef402c5c13afbf6235221eccf2414a53358b0c
Monwer, Tarek <u1138824@internal.imsglobal.com>
2024-03-05 23:31:14 +0600

    second migration

--- Migrations/20240305173008_AddSearchCountToWeather.Designer.cs
+++ Migrations/20240305173008_AddSearchCountToWeather.Designer.cs
@@ -4,6 +4,7 @@ using ExperimentNetApi6.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExperimentNetApi6.Migrations
 {
     [DbContext(typeof(ExperimentNetApi6Context))]
-    partial class ExperimentNetApi6ContextModelSnapshot : ModelSnapshot
+    [Migration("20240305173008_AddSearchCountToWeather")]
+    partial class AddSearchCountToWeather
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240305173008_AddSearchCountToWeather.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExperimentNetApi6.Migrations
+{
+    public partial class AddSearchCountToWeather : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SearchCount",
+                table: "Weather",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SearchCount",
+                table: "Weather");
+        }
+    }
+}
