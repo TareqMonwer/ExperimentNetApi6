a7a037eece45b674d761e57f8d796e35c58df384
Monwer, Tarek <u1138824@internal.imsglobal.com>
2024-03-05 15:51:45 +0600

    initial commit

---
+++ Data/ExperimentNetApi6Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExperimentNetApi6.Data
+{
+    public class ExperimentNetApi6Context : DbContext
+    {
+        IConfiguration appConfig;
+
+        public DbSet<Weather> Weather { get; set; }
+
+        public ExperimentNetApi6Context(IConfiguration config)
+        {
+            appConfig = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(appConfig.GetConnectionString("ExperminetNet6"));
+        }
+
+    }
+}
---
+++ Data/Weather.cs
@@ -0,0 +1,11 @@
+﻿namespace ExperimentNetApi6.Data
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public int TemperatureC { get; set; }
+        public int TemperatureF { get; set; }
+        public string Summary { get; set; }
+    }
+}
